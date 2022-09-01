c1e3c89a01e0affdf0991a0619e0a8bab1cb7d35
Poq Xert <git@poqxert.ru>
2022-09-01 15:04:00 +1000

    Implement a patch system.

--- editor/export/editor_export.cpp
+++ editor/export/editor_export.cpp
@@ -82,6 +82,7 @@ void EditorExport::_save() {
 		config->set_value(section, "include_filter", preset->get_include_filter());
 		config->set_value(section, "exclude_filter", preset->get_exclude_filter());
 		config->set_value(section, "export_path", preset->get_export_path());
+		config->set_value(section, "patches", preset->get_patches());
 		config->set_value(section, "encryption_include_filters", preset->get_enc_in_filter());
 		config->set_value(section, "encryption_exclude_filters", preset->get_enc_ex_filter());
 		config->set_value(section, "encrypt_pck", preset->get_enc_pck());
@@ -286,6 +287,12 @@ void EditorExport::load_config() {
 		preset->set_exclude_filter(config->get_value(section, "exclude_filter"));
 		preset->set_export_path(config->get_value(section, "export_path", ""));
 		preset->set_script_export_mode(config->get_value(section, "script_export_mode", EditorExportPreset::MODE_SCRIPT_BINARY_TOKENS_COMPRESSED));
+
+		Vector<String> patch_list = config->get_value(section, "patches", Vector<String>());
+
+		for (const String &patch : patch_list) {
+			preset->add_patch(patch);
+		}
 
 		if (config->has_section_key(section, "encrypt_pck")) {
 			preset->set_enc_pck(config->get_value(section, "encrypt_pck"));
